d996e45a541e7b3902254261feee6b47ae2bf205
osexton1 <odhran.sexton@gmail.com>
2023-11-08 16:00:16 +0000

    Created ToDoHeader and new styles

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import ToDoHeader from './components/ToDoHeader.vue';
 </script>
 
 <template>
+  <ToDoHeader />
   <RouterView />
 </template>
 
