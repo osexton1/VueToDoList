aee3e101e1a070b99d5437aa957437e17ea9053d
osexton1 <odhran.sexton@gmail.com>
2023-11-08 16:38:40 +0000

    Changed file name and added ToDosCreator

---
+++ src/components/ToDoCreator.vue
@@ -0,0 +1,47 @@
+<script setup>
+    import { defineEmits, reactive } from 'vue';
+
+    const todoState = reactive({
+        todo: "",
+        invalid: null,
+        errMsg: ""
+    });
+    const emit = defineEmits(["create-todo"])
+
+    const createTodo = () => {
+        emit("create-todo", todoState.todo);
+    }
+</script>
+
+<template>
+    <div class="input-wrap">
+        <input type="text" v-model="todoState.todo"/>
+        <button @click="createTodo">Create</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .input-wrap {
+        display: flex;
+        transition: 250ms ease;
+        border: 2px solid #41b080;
+        &:focus-within {
+            box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
+                0 -2px 4px -2px rgb(0 0 0 / 0.1);
+        }
+
+        input {
+            width: 100%;
+            padding: 8px 6px;
+            border: none;
+            &:focus {
+                outline: none;
+            }
+        }
+
+        button {
+            padding: 8px 16px;
+            border: none;
+        }
+    }
+</style>
